7ccb8ab387933975f9b1c9c2ddb14e884ec4b67f
Lev Zakharov <l.j.zakharov@gmail.com>
2018-06-11 08:48:16 +0300

    Rename project

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-GitList
+gitls
 =======
 
 Command line tool for listing git repositories on your machine.
--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='gitlist',
+    name='gitls',
     version='0.0.1',
     description='Command line tool for listing git repositories on your machine.',
     long_description=readme,
     author='Lev Zakharov',
     author_email='l.j.zakharov@gmail.com',
-    url='https://github.com/lzakharov/gitlist',
+    url='https://github.com/lzakharov/gitls',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
-        'console_scripts': ['gitlist=gitlist.cli:main']
+        'console_scripts': ['gitls=gitls.cli:main']
     }
 )
